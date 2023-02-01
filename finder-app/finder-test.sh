718f481b28f56650e589dbd2f7c4b58135f8528d
ericksyndrome <erick.reyes10@yahoo.com>
2023-02-01 12:32:12 -0600

    pls work

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -48,15 +48,15 @@ then
 		exit 1
 	fi
 fi
-<<<<<<< HEAD
-#echo "Removing the old writer utility and compiling as a native application"
-#make clean
-#make
-=======
+
 echo "Removing the old writer utility and compiling as a native application"
 make clean
 make
->>>>>>> second assign
+
+echo "Removing the old writer utility and compiling as a native application"
+make clean
+make
+
 
 for i in $( seq 1 $NUMFILES)
 do
