de45ab46a983e6c1a26cbfb38dc601177f5dce8f
ericksyndrome <erick.reyes10@yahoo.com>
2023-02-24 12:39:06 -0600

    help

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -57,10 +57,10 @@ echo "Removing the old writer utility and compiling as a native application"
 
 for i in $( seq 1 $NUMFILES)
 do
-	writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	/usr/bin/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(/usr/bin/finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
