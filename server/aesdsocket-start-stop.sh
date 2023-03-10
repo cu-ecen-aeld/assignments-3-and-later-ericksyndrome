3f7e76f6d5590408e90cc8c6313a7038ced01d5f
ericksyndrome <erick.reyes10@yahoo.com>
2023-03-10 08:40:35 -0600

    pls pls pls

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,17 +1,17 @@
-#!/bin/sh
+#! /bin/sh
 
 case "$1" in
 	start)
-	echo "Starting aesdsocket"
-	start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
-	;;
+		echo "Starting aesdsocket"
+		start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket
+		;;
 	stop)
-	echo "Stopping aesdsocket"
-	start-stop-daemon -K -n aesdsocket
-	;;
+		echo "Stopping aesdsocket"
+		start-stop-daemon -K -n aesdsocket
+		;;
 	*)
-	echo "Usage: $0 {start | stop}"
-	exit 1
-
+		echo "Usage: $0 {start | stop}"
+		exit 1
 esac
-exit 1
+
+exit 0
